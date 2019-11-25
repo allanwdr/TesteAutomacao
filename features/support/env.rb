0aad9c66429267bba2d275ef97fa79a1d8f528f3
Allan Rodrigues <allanwdr@gmail.com>
2019-11-25 16:06:06 -0300

    Adicionado a URL da Web no arquivo de config e pego via YAML

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,16 @@
-require 'capybara/cucumber'
+require 'yaml'
 require 'selenium-webdriver'
+require 'capybara/cucumber'
+require 'site_prism'
 require 'rspec'
 require 'pry'
 require 'ffi' if Gem.win_platform?
 
+YAML_DATA = YAML.load_file("#{File.dirname(__FILE__)}/data/test_data.yaml")
+CONFIG_DATA = YAML.load_file("#{File.dirname(__FILE__)}/config/config.yaml")
+
 BROWSER = ENV['BROWSER']
 
-# binding.pry
 if ENV['PLATAFORM'].eql?('web')
   case BROWSER
   when 'chrome' then Selenium::WebDriver::Chrome::Service.driver_path = "#{File.dirname(__FILE__)}/drivers/chromedriver.exe"
@@ -15,22 +19,21 @@ if ENV['PLATAFORM'].eql?('web')
   end
 
   Capybara.configure do |config|
-    config.app_host = 'https://www.inmetrics.com.br/'
+    config.app_host = CONFIG_DATA['urls']['web']
     config.default_max_wait_time = 10
     config.default_driver = case BROWSER
                             when 'CHROME' then :chrome
                             when 'FIREFOX' then :firefox
                             when 'internet_explorer' then :ie
+                            end
   end
 elsif ENV['PLATAFORM'].eql?('api')
   HTTP_LOG = ENV['HTTP_LOG'].eql?('true')
 else
   raise %(##################################################################
 
-Deve ser informado o sistema que deseja executar, ex.:
+Deve ser informado a plataforma que deseja executar, ex.:
 cucumber -t @confira_nossas_vagas -p web
 
 ##################################################################)
 end
-# binding.pry
-
