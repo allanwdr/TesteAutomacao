63cf7ddd81318787095774ed1d4ea4e9ae57a984
Allan Rodrigues <allanwdr@gmail.com>
2019-11-25 14:27:03 -0300

    Adicionado retorno do cucumber para executar o projeto

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,11 +2,35 @@ require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'rspec'
 require 'pry'
+require 'ffi' if Gem.win_platform?
 
+BROWSER = ENV['BROWSER']
 
-driver_path = Dir[File.join(File.dirname(__FILE__), 'drivers/')]
+# binding.pry
+if ENV['PLATAFORM'].eql?('web')
+  case BROWSER
+  when 'chrome' then Selenium::WebDriver::Chrome::Service.driver_path = "#{File.dirname(__FILE__)}/drivers/chromedriver.exe"
+  when 'firefox' then Selenium::WebDriver::Firefox::Service.driver_path = "#{File.dirname(__FILE__)}/drivers/geckodriver.exe"
+  when 'internet_explorer' then Selenium::WebDriver::IE::Service.driver_path = "#{File.dirname(__FILE__)}/drivers/IEDriverServer.exe"
+  end
 
-binding.pry
-case ENV['BROWSER']
-when 'CHROME' then Selenium::WebDriver::Chrome::Service.driver_path = "#{File.dirname(__FILE__)}/drivers/chromedriver.exe"
-when 'FIREFOX' then Selenium::WebDriver::Firefox::Service.driver_path = "#{File.dirname(__FILE__)}/drivers/geckodriver.exe"
+  Capybara.configure do |config|
+    config.app_host = 'https://www.inmetrics.com.br/'
+    config.default_max_wait_time = 10
+    config.default_driver = case BROWSER
+                            when 'CHROME' then :chrome
+                            when 'FIREFOX' then :firefox
+                            when 'internet_explorer' then :ie
+  end
+elsif ENV['PLATAFORM'].eql?('api')
+  HTTP_LOG = ENV['HTTP_LOG'].eql?('true')
+else
+  raise %(##################################################################
+
+Deve ser informado o sistema que deseja executar, ex.:
+cucumber -t @confira_nossas_vagas -p web
+
+##################################################################)
+end
+# binding.pry
+
